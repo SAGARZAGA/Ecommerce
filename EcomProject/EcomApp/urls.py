b2631423baad3ef2870cba83e5a7dd5d43fe5b3a
SAGARZAGA <102744475+SAGARZAGA@users.noreply.github.com>
2022-08-17 20:25:55 +0530

    Update urls.py

--- EcomProject/EcomApp/urls.py
+++ EcomProject/EcomApp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('',views.AllProductCat,name= 'AllProductCat'),
     path('<slug:c_slug>', views.AllProductCat, name= 'product_by_category') ,
     path('<slug:c_slug>/<slug:product_slug>/', views.productDetails, name= 'productDetails') ,
+    path('buy/', views.buy, name= 'buy') ,
 ]
